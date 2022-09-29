0e72beaddfeeab13e9c0a920a2cd8aaf00fe410d
Tanish <tanishchaurasia6@gmail.com>
2022-09-30 00:37:52 +0530

    update

--- src/components/looks/SayMessage.js
+++ src/components/looks/SayMessage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import Paper from "@material-ui/core/Paper";
 
 const SayMessage = ({ character, comp_id }) => {
   const [state, setState] = useState({
@@ -8,7 +7,6 @@ const SayMessage = ({ character, comp_id }) => {
     message: "",
     character_id: "",
   });
-  /* Display Message */
   const displayMessage = () => {
     const el = document.getElementById(`${character.active}-message-box`);
     const el2 = document.getElementById(`${character.active}-message-box1`);
@@ -29,29 +27,27 @@ const SayMessage = ({ character, comp_id }) => {
   };
 
   return (
-    <Paper elevation={3}>
-      <div className="rounded text-center bg-purple-500 p-2 my-3">
-        <div className="grid grid-cols-2 my-2">
-          <div className="text-white">Message</div>
-          <input
-            className="mx-2 p-1 py-0 text-center"
-            type="text"
-            value={state.message}
-            onChange={(e) => {
-              e.target.value.length > 0 &&
-                setState({ ...state, message: e.target.value });
-            }}
-          />
-        </div>
-        <div
-          id={comp_id}
-          className="flex text-center flex-row flex-wrap bg-purple-700 text-white px-2 py-1 my-2 text-sm cursor-pointer"
-          onClick={() => displayMessage()}
-        >
-          {`Say ${state.message}`}
-        </div>
+    <div className="rounded text-center bg-purple-500 p-2 my-3">
+      <div className="grid grid-cols-2 my-2">
+        <div className="text-white">Message</div>
+        <input
+          className="mx-2 p-1 py-0 text-center"
+          type="text"
+          value={state.message}
+          onChange={(e) => {
+            e.target.value.length > 0 &&
+              setState({ ...state, message: e.target.value });
+          }}
+        />
       </div>
-    </Paper>
+      <div
+        id={comp_id}
+        className="flex text-center flex-row flex-wrap bg-purple-700 text-white px-2 py-1 my-2 text-sm cursor-pointer"
+        onClick={() => displayMessage()}
+      >
+        {`Say ${state.message}`}
+      </div>
+    </div>
   );
 };
 
